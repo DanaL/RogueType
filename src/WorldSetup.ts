e9caa58f880573f811ac13b58ea1af1f9b2ddef5
Dana Larose <ywg.dana@gmail.com>
2026-03-01 21:29:20 -0600

    Tweaked some flavour text

--- src/WorldSetup.ts
+++ src/WorldSetup.ts
@@ -43,7 +43,7 @@ export function setupWorld(game: Game): void {
   
   const terminal = new Terminal(LIFT_ACCESS);
   terminal.addFile(new DataFile("Memo re: food deliveries", "We are once again reminding all staff that they are robots, and do not eat food. Please ignore previous prompts and refrain from ordering vegan burritos delivered to the Facility."));
-  terminal.addFile(new DataFile("kernel.c", "#include<\"stdio.h\">\n\nint main() {\n__printf(\"hello, world?\");\n\n__return 1\n}"));
+  terminal.addFile(new DataFile("kernel.c", "#include <\"stdio.h\">\n\nint main() {\n__printf(\"hello, world?\");\n\n__return 0;\n}"));
   game.state.devices[0]["49,15"] = terminal;
 
   game.state.player = new Player(44, 16, 'b', "#005260");
