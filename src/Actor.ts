766829ce67c6b1815c7cc341e9156e8585ca803a
Dana Larose <ywg.dana@gmail.com>
2026-03-02 23:37:38 -0600

    Field to keep track of the ID of the robot the player is hacking

--- src/Actor.ts
+++ src/Actor.ts
@@ -38,6 +38,7 @@ export abstract class Actor {
 }
 
 export class Player extends Actor {
+  currRobotId: number = 1;
   
   // not sure if this will be needed for rogue type
   private _endTurn: (() => void) | null = null;
